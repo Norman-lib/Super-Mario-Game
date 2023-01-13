198b502f6360e8bf60dbc3e3da494dd1ea80757c
Majid Hamza <hamzamajidtmnttmnt@gmail.com>
2023-01-13 18:00:14 +0100

    textures

--- PLayer and world/main.cpp
+++ PLayer and world/main.cpp
@@ -5,10 +5,18 @@
 #include <string>;
 #include <iostream>
 #include <irrKlang.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 
 using namespace std;
 
 using namespace irrklang;
+
+//texture vars;
+unsigned char* marioLeftData;
+int marioWidth, marioHeight, marioNrChannels;
+
 
 
 string coinSoundstr = "sounds\\coin.ogg";
@@ -22,7 +30,6 @@ vector<float> gravity = { 0, -0.5 };
 vector<float> platformPos = { 0,-1 };
 vector<float> platformDim = { 10.0,0.5 };
 vector<vector<vector<float>>*> obstacles;
-
 
 vector<float> platform2Pos = { 5,2 };
 vector<float> platform2Dim = { 5.0,0.5 };
@@ -43,6 +50,10 @@ float lastTime = 0.0f;
 float radius = 0.5;
 vector<vector<float>> goldPos = { {-5,0},{2, 1} };
 
+float enemyWidth = 0.5f;
+vector<vector<float>> enemyPos = { {-5, 0} , {3, 3} };
+vector<vector<float>> enemiesSpeed = { {moveForce, 0}, {moveForce,0} };
+
 void drawGold(vector<vector<float>> g) {
     
     for (int j = 0; j < goldPos.size(); j++) {
@@ -57,6 +68,42 @@ void drawGold(vector<vector<float>> g) {
         
 }
 
+
+GLuint textureMarioLeft;
+
+void loadTexture(const char* filename) {
+    int width, height, numComponents;
+    unsigned char* imageData = stbi_load(filename, &width, &height, &numComponents, 0);
+
+    if (imageData == NULL)
+        std::cerr << "Unable to load texture: " << filename << std::endl;
+    else {
+        cout<<"worked" << endl;
+        cout << "width :" << width << endl;
+        cout << "width :" << height << endl;
+        cout << "width :" << numComponents << endl;
+
+    }
+    // Generate the texture object
+    glGenTextures(1, &textureMarioLeft);
+    glBindTexture(GL_TEXTURE_2D, textureMarioLeft);
+
+    // Set texture parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // Copy the image data to the texture object
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // Free the image data
+    stbi_image_free(imageData);
+}
+
+
+
+
 void drawRectangle(vector<float> pos, vector<float> dim ) {
     glBegin(GL_QUADS);
     glVertex2f(pos[0] - dim[0], pos[1] - dim[1]);
@@ -65,6 +112,8 @@ void drawRectangle(vector<float> pos, vector<float> dim ) {
     glVertex2f(pos[0] + dim[0], pos[1] - dim[1]);
     glEnd();
 }
+
+
 
 enum CollisionSide {
     None,
@@ -141,6 +190,44 @@ bool DetectCollisionWPlayer(vector<float> targetPos, vector<float> targetDims) {
         playerPos[0] + playerDim[0] > targetPos[0] - targetDims[0];
 }
 
+CollisionSide checkEnemyCollision(vector<float> playerPos, vector<float> playerDim, vector<float> platformPos, vector<float> platformDim) {
+    
+    float playerLeft = playerPos[0] - playerDim[0];
+    float playerRight = playerPos[0] + playerDim[0];
+    float playerTop = playerPos[1] + playerDim[1];
+    float playerBottom = playerPos[1] - playerDim[1];
+
+    // Calculate the edges of the platform rectangle
+    float platformLeft = platformPos[0] - platformDim[0];
+    float platformRight = platformPos[0] + platformDim[0];
+    float platformTop = platformPos[1] + platformDim[1];
+    float platformBottom = platformPos[1] - platformDim[1];
+
+    // Check for collision on the left top side
+    if (playerRight > platformLeft && playerLeft < platformLeft
+        && abs(playerBottom - platformTop) < 0.1
+       //left
+        //top
+       ) {
+        return Left;
+
+    }
+
+    // Check for collision on the right top side
+    if (playerLeft < platformRight && playerRight > platformRight
+        && abs(playerBottom - platformTop) <0.1
+        
+        //top
+       /* playerBottom < platformTop && playerTop > platformTop &&
+        playerRight > platformLeft && playerLeft < platformRight*/
+        ) {
+       
+        return Right;
+    }
+    // if nothing return None
+    return None;
+}
+
 void updatePlayerPosition() {
     // Apply gravity
     velocity[1] += gravity[1];
@@ -201,11 +288,151 @@ void updatePlayerPosition() {
        
 
 }
+
+
+void updateEnemiesPosition() {
+    for (int j = 0; j < enemyPos.size(); j++) {
+        enemiesSpeed[j][1] += gravity[1];
+
+
+       CollisionSide side2 = checkEnemyCollision(enemyPos[j], { enemyWidth, enemyWidth }, platform2Pos, platform2Dim);
+       CollisionSide side = checkEnemyCollision(enemyPos[j], { enemyWidth, enemyWidth }, platformPos, platformDim);
+
+
+        //moveleft and right for enemies
+        if ( side == Left || side2 == Left) {
+            enemiesSpeed[j][0] = moveForce;
+        }
+        if (side == Right || side2 == Right) {
+
+            enemiesSpeed[j][0] = -moveForce;
+        }
+
+        // Update player position based on velocity
+        enemyPos[j][0] += enemiesSpeed[j][0] * deltaTime;
+        enemyPos[j][1] += enemiesSpeed[j][1] * deltaTime;
+
+
+        Collision collision = checkCollision(enemyPos[j], { enemyWidth , enemyWidth}, platformPos, platformDim);
+        Collision collision2 = checkCollision(enemyPos[j], { enemyWidth , enemyWidth }, platform2Pos, platform2Dim);
+        vector<Collision> cols = { collision, collision2 };
+        for (int i = 0; i < cols.size(); i++)
+        {
+            if (cols[i].side != None) {
+                if (cols[i].side == Top) {
+                    enemyPos[j][1] = cols[i].pos[1] + cols[i].dim[1] + enemyWidth;
+                    enemiesSpeed[j][1] = 0;
+                    
+                }
+                else if (cols[i].side == Bottom) {
+                    enemyPos[j][1] = cols[i].pos[1] - cols[i].dim[1] - enemyWidth;
+                    enemiesSpeed[j][1] = 0;
+                }
+                else if (cols[i].side == Left) {
+                    enemyPos[j][0] = cols[i].pos[0] - cols[i].dim[0] - enemyWidth;
+                    enemiesSpeed[j][0] = 0;
+                }
+                else if (cols[i].side == Right) {
+                    enemyPos[j][0] = cols[i].pos[0] + cols[i].dim[0] + enemyWidth;
+                    enemiesSpeed[j][0] = 0;
+                }
+            }
+        }
+
+
+    }
+
+
+    // Apply gravity
+    //velocity[1] += gravity[1];
+
+
+   
+   
+     /*if (right1) {
+        velocity[0] = moveForce;
+    }
+    else if (left1) {
+        velocity[0] = -moveForce;
+    }
+    else {
+        velocity[0] = 0;
+    }*/
+    // Update player position based on velocity
+   /* playerPos[0] += velocity[0] * deltaTime;
+    playerPos[1] += velocity[1] * deltaTime;*/
+
+    // Check for collision with platform
+    //if (DetectCollisionWPlayer(platformPos, platformDim)) {
+
+    //    // Set player position to top of platform
+    //    playerPos[1] = platformPos[1] + platformDim[1] + playerDim[1];
+    //    velocity[1] = 0;
+    //    canJump = true;
+    //}
+    // Check for collision with platform
+   /* Collision collision = checkCollision(playerPos, playerDim, platformPos, platformDim);
+    Collision collision2 = checkCollision(playerPos, playerDim, platform2Pos, platform2Dim);
+    vector<Collision> cols = { collision, collision2 };
+    for (int i = 0; i < cols.size(); i++) 
+    {
+        if (cols[i].side != None) {
+            if (cols[i].side == Top) {
+                playerPos[1] = cols[i].pos[1] + cols[i].dim[1] + playerDim[1];
+                velocity[1] = 0;
+                canJump = true;
+            }
+            else if (cols[i].side == Bottom) {
+                playerPos[1] = cols[i].pos[1] - cols[i].dim[1] - playerDim[1];
+                velocity[1] = 0;
+            }
+            else if (cols[i].side == Left) {
+                playerPos[0] = cols[i].pos[0] - cols[i].dim[0] - playerDim[0];
+                velocity[0] = 0;
+            }
+            else if (cols[i].side == Right) {
+                playerPos[0] = cols[i].pos[0] + cols[i].dim[0] + playerDim[0];
+                velocity[0] = 0;
+            }
+        }
+    }*/
+
+
+}
+
+void drawPlayerWithTexture(vector<float> pos, vector<float> dim) {
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    // Enable texture coordinates
+    glEnable(GL_TEXTURE_2D);
+
+    // Bind the texture
+    glBindTexture(GL_TEXTURE_2D, textureMarioLeft);
+
+    // Draw the shape
+    glBegin(GL_QUADS);
+    // Set the texture coordinates for each vertex
+    glTexCoord2f(0.0f, 0.0f);
+    glVertex2f(pos[0] + dim[0], pos[1] + dim[1]);
+    glTexCoord2f(1.0f, 0.0f);
+    glVertex2f(pos[0] - dim[0], pos[1] + dim[1]);
+    glTexCoord2f(1.0f, 1.0f);
+    glVertex2f(pos[0] - dim[0], pos[1] - dim[1]);
+    glTexCoord2f(0.0f, 1.0f);
+    glVertex2f(pos[0] + dim[0], pos[1] - dim[1]);
+    glEnd();
+
+    // Unbind the texture
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+}
+
 void timer(int value) {
     currentTime = glutGet(GLUT_ELAPSED_TIME);
     deltaTime = (currentTime - lastTime) / 1000.0f;
     lastTime = currentTime;
     updatePlayerPosition();
+    updateEnemiesPosition();
     glutPostRedisplay();
     glutTimerFunc(1000 / 60, timer, 0);
 }
@@ -234,7 +461,9 @@ void displayScore() {
 
 void display()
 {
+    
     updatePlayerPosition();
+    updateEnemiesPosition();
 
    
 
@@ -256,9 +485,9 @@ void display()
     glVertex2f(-18.0f, -18.0f);
     glEnd();
 
+
     // Draw the player square
-    glColor3f(0.0f, 1.0f, 0.0f);
-    drawRectangle(playerPos, playerDim);
+    drawPlayerWithTexture(playerPos, playerDim);
 
     glColor3f(1.0, 0.843137, 0.0);
     drawGold(goldPos);
@@ -268,6 +497,14 @@ void display()
     glColor3f(1.0f, 1.0f, 0.0f);
     drawRectangle(platformPos, platformDim);
     drawRectangle(platform2Pos, platform2Dim);
+
+
+
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    for (int i = 0; i < enemyPos.size(); i++) {
+        drawRectangle(enemyPos[i], { enemyWidth, enemyWidth });
+    }
 
     glColor3f(1.0f, 1.0f, 1.0f);
     displayScore();
@@ -316,7 +553,7 @@ int main(int argc, char** argv)
     themeSongstr = "sounds\\theme.ogg";
     const char* themeSong = themeSongstr.c_str();
     ISoundEngine* SoundEngine = createIrrKlangDevice();
-    SoundEngine->play2D(themeSong, true);
+   // SoundEngine->play2D(themeSong, true);
     
     //const char* c= &music;
     //SoundEngine->play2D(s, false);
@@ -325,8 +562,10 @@ int main(int argc, char** argv)
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(800, 800);
     glutCreateWindow("Player and World");
+    glClearColor(0.49, 0.47, 0.87, 1.0f);
     glutKeyboardFunc(keyboard);
     glutKeyboardUpFunc(keyboardUp);
+    loadTexture("marioLeft.jpg");
     glutDisplayFunc(display);
 
     glutTimerFunc(1000 / 60, timer, 0);
